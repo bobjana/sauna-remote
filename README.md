fc3fec1f25c5e55ee4116fe4d0dd466207cdba20
Dylan Viljoen <dviljoen@bol.com>
2020-04-19 19:40:34 +0200

    dhtesp over Blynk

---
+++ README.md
@@ -0,0 +1,16 @@
+# SAUNA
+
+
+
+### Spike:
+
+* led blink
+* dht11
+* wifi
+* oled
+* mqqt
+* thinger.io integration
+* blynk integration
+* 3v -> 5v
+* relay switches
+* voltage regulator
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,51 @@
-#include <Arduino.h>
+  
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <SimpleTimer.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#include "DHTesp.h"
+
+#ifdef ESP32
+#pragma message(THIS EXAMPLE IS FOR ESP8266 ONLY!)
+#error Select ESP8266 board.
+#endif
+
+SimpleTimer timer;
+char auth[] = "ONSxsBUvzdBn5-SES-ZZfEKUuziaaPID";
+char ssid[] = "Ziggo8655252";
+char pass[] = "nyDgvy7H6smv";
+float t;
+float h;
+
+DHTesp dht;
+
+void sendUptime()
+{
+  float h = dht.getHumidity();
+  float t = dht.getTemperature();
+  Serial.println("Humidity and temperature\n\n");
+  Serial.print("Current humidity = ");
+  Serial.print(h);
+  Serial.print("%  ");
+  Serial.print("temperature = ");
+  Serial.print(t); 
+  Blynk.virtualWrite(V0, t);
+  Blynk.virtualWrite(V1, h); 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  Serial.begin(115200);
+  Blynk.begin(auth, ssid, pass);
+  dht.setup(D4, DHTesp::DHT11);
+  timer.setInterval(2000, sendUptime);
+  Serial.println();
+  Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)\tHeatIndex (C)\t(F)");
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
 }
