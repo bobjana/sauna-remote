f225843073751ca455d22ffda2d3261f81c491bd
Dylan Viljoen <dviljoen@bol.com>
2020-04-19 19:53:46 +0200

    Add RGB led

--- src/main.cpp
+++ src/main.cpp
@@ -18,10 +18,23 @@ char pass[] = "nyDgvy7H6smv";
 float t;
 float h;
 
+int red_light_pin= D6;
+int green_light_pin = D5;
+int blue_light_pin = D0;
+
 DHTesp dht;
+
+void RGB_color(int red_light_value, int green_light_value, int blue_light_value)
+ {
+  analogWrite(red_light_pin, red_light_value);
+  analogWrite(green_light_pin, green_light_value);
+  analogWrite(blue_light_pin, blue_light_value);
+}
 
 void sendUptime()
 {
+  RGB_color(255, 0, 0);
+
   float h = dht.getHumidity();
   float t = dht.getTemperature();
   Serial.println("Humidity and temperature\n\n");
@@ -40,6 +53,11 @@ void setup()
   Blynk.begin(auth, ssid, pass);
   dht.setup(D4, DHTesp::DHT11);
   timer.setInterval(2000, sendUptime);
+
+  pinMode(red_light_pin, OUTPUT);
+  pinMode(green_light_pin, OUTPUT);
+  pinMode(blue_light_pin, OUTPUT);
+
   Serial.println();
   Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)\tHeatIndex (C)\t(F)");
 }
@@ -49,3 +67,4 @@ void loop()
   Blynk.run();
   timer.run();
 }
+
